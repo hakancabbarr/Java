e5352c01200ee29247b319acc9262f9b29cad7b8
hakancabbarr <114572701+hakancabbarr@users.noreply.github.com>
2022-10-06 10:33:46 +0300

    Update ArtikYil.java

--- ArtikYil.java
+++ ArtikYil.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 
-public class ArtikYil {
+public class artikYil {
     public static void main(String[] args) {
         int yil;
 
-        Scanner inp = new Scanner(System.in);
-        System.out.print("Yıl Giriniz:");
-        yil = inp.nextInt();
+        Scanner input=new Scanner(System.in);
+        System.out.println("Yıl giriniz:");
+        yil= input.nextInt();
 
-        if ((yil % 4 == 0) && (yil % 100 != 0) || (yil % 400 == 0)) {
-            System.out.println(yil + " yılı artık yıldır");
-        } else {
-            System.out.println(yil + " yılı artık yıl değildir");
+        if(yil%4==0 || yil%400==0){
+            System.out.println(+yil+ " bir artık yıldır!");
+        }else{
+            System.out.println(+yil+ " bir artık yıl değildir!");
         }
+
     }
 }
