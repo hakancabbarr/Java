27ee9be01986323dafa720bea2455c2d947cb0ba
hakancabbarr <114572701+hakancabbarr@users.noreply.github.com>
2022-10-11 16:55:12 +0300

    Create UcgenlerleElmas.java

---
+++ UcgenlerleElmas.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class UcgenlerleElmas {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Bir Sayı Giriniz :");
+        int n = inp.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j > (i-n); j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+
+    }
+
+}
